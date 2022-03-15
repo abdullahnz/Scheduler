9a377fb8e968131e582c2022f0d6157d4575489d
abdullahnz <edfallennzm@gmail.com>
2022-03-15 11:21:52 +0700

    Fix bug am/pm

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Convert `Jadwal Mahasiswa`'s data from igracias telkom university to Google Cale
 
 1. Login with your account into Igracias, goto Registration > Schedule > Student Schedule.
 2. Inspect element or press `ctrl + shift + i`, goto tap Network and press `ctrl + r` for reload.
-3. Click `Fetch/XHR` and search for `/libraries/ajax/ajax.schedule.php?act=viewStudentSchedule&studentId=...` and click to tap `Response`.
+3. Click `Fetch/XHR` and search for `ajax.schedule.php?act=viewStudentSchedule&studentId=...` and click to tap `Response`.
 4. You will see some data like this (or see example.json), ![data](assets/get.png)
 5. Just copy all response data and save into `data.json`
 6. Run `scheduler.py`.
@@ -14,11 +14,17 @@ Convert `Jadwal Mahasiswa`'s data from igracias telkom university to Google Cale
     # install all required modules
     $ python3 -m pip install -r requirements.txt
     
-    # pass total weeks at argv[1]
-    $ python3 scheduler.py <total weeks>
+    # see advanced option
+    $ python3 scheduler.py -h
 
-    # run without total weeks will run with 1 week.
+    # default
     $ python3 scheduler.py
+    
+    # with custom input json file
+    $ python3 scheduler.py -in <infile>
+    
+    # with custom output ics file
+    $ python3 scheduler.py -out <outfile>
     ```
 7. You'll see `schedule.ics` file.
 8. The last step is you need to import `schedule.ics` to your google calendar.
--- scheduler.py
+++ scheduler.py
@@ -1,60 +1,84 @@
 #!/usr/bin/python3
+  
+from icalendar import Calendar, Event
+import datetime, argparse
+import json 
 
-from calendar import calendar
-from ics import Calendar, Event
-import datetime, arrow
-import json, sys
-
-# https://stackoverflow.com/questions/42950/how-to-get-the-last-day-of-the-month
-def last_day_of_month(date):
-    if date.month == 12:
-        return date.replace(day=31)
-    return date.replace(month=date.month+1, day=1) - datetime.timedelta(days=1)
-    
-def parse_hour(time):
-    return map(int, time.split(':'))
-
-def process_time(date, time, counter = 0):
-    hour, minute, second = parse_hour(time)
-    return arrow.get(datetime.datetime(year=date[0], day=date[1], month=date[2],
-                                    hour=hour, minute=minute, second=second))
-
-schedule = json.loads(open('data.json', 'rb').read())['aaData']
-
-current_date = datetime.datetime.now()
-
-year  = current_date.year
-day   = current_date.day - datetime.datetime.today().weekday()
-month = current_date.month
-
-calendar = Calendar()
-prev_day = 'SENIN'
-
-n_weeks = int(sys.argv[1]) if len(sys.argv) > 1 else 1
-
-for _ in range(n_weeks):
-    for data in schedule:
-        if prev_day != data[0]:
-            prev_day = data[0]
-            day += 1
-
-        month_range = last_day_of_month(datetime.date(year, month, 1)).day
+class Scheduler:
+    def __init__(self, filename = None):
+        current_date = datetime.datetime.now()
         
-        if day > month_range:
-            day = day % month_range
-            month += 1
+        self.filename = "data.json" if filename == None else filename
+        self.year  = current_date.year
+        self.day   = current_date.day - datetime.datetime.today().weekday()
+        self.month = current_date.month
+        self.prev  = "SENIN"
 
-        if month > 12:
-            month = 1
-            year += 1
+    def parse_time(self, time):
+        hour, minute, second = map(int, time.split(':'))
+        return datetime.datetime(year=self.year, day=self.day, month=self.month,
+                                    hour=hour, minute=minute, second=second)
+        
+    def last_day_of_month(self, date):
+        if date.month == 12:
+            return date.replace(day=31)
+        return date.replace(month=date.month+1, day=1) - datetime.timedelta(days=1)
 
-        event = Event()
-        event.name  = f'{data[4]} ({data[5]})'
-        event.description = f'''Ruang: {data[2]}\nKelas: {data[6]}\nCourse ID: {data[3]}'''
-        event.begin = process_time((year, day, month), data[1])
-        event.end   = process_time((year, day, month), data[7])
+    def check_date(self, course_day):
+        range_day_of_month = self.last_day_of_month(datetime.date(self.year, self.month, 1)).day
+        
+        if self.prev != course_day:
+            self.day += 1
 
-        calendar.events.add(event)
+        if self.day > range_day_of_month:
+            self.day = self.day % range_day_of_month
+            self.month += 1
 
-with open('schedule.ics', 'w') as scheduler:
-    scheduler.writelines(calendar)
+        if self.month > 12:
+            self.month = 1
+            self.year += 1
+
+        return course_day
+    
+    def parse(self, info, outfile):
+        schedule = open(self.filename, 'rb').read()
+        schedule = json.loads(schedule)['aaData']
+
+        calendar = Calendar()
+       
+        for course_day, start, room, course_id, course, lecturer_id, grade, end, stat in schedule:
+            self.prev = self.check_date(course_day)
+
+            summary = f'{course} ({lecturer_id})'
+            description    = f'Ruang: {room} | Kelas: {grade} | Course ID: {course_id}'
+            weekly_freq = {'freq': 'weekly'}
+            
+            event = Event()
+            event.add('summary', summary)
+            event.add('description', description)
+            event.add('dtstart', self.parse_time(start))
+            event.add('dtend', self.parse_time(end))
+            event.add('rrule', weekly_freq)
+            
+            if info:
+                print(f'Adding event {course} in {course_day} at {start} - {end} ({self.day}-{self.month}-{self.year})')
+            
+            calendar.add_component(event)
+
+        with open(outfile, 'wb') as scheduler:
+            scheduler.write(calendar.to_ical())
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Used for convert igracias\' student subject to icalendar.ics file.')
+    parser.add_argument('-in', '--inp', action='store', type=str, help='select input file of subject data (default: data.json)') 
+    parser.add_argument('-out', '--out', action='store', type=str, help='select output file for ics file (default: schedule.ics)') 
+    parser.add_argument('--info', action='store', type=int, help='print information to stdout (default=1)') 
+    args = parser.parse_args()
+
+    infile  = args.inp if args.inp else 'data.json'
+    outfile = args.out if args.out else 'schedule.ics'
+    info    = args.info if args.info else 1
+
+    scheduler = Scheduler(filename = infile)
+    scheduler.parse(info = info, outfile = outfile)
+    
